64c207ba96956912816beebd7429f7646f18c23f
serah shani <serahshani035@gmail.com>
2024-07-04 15:08:37 +0300

    strings

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'GRACE HOPER';
+const welcomeMessage =`Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.slice(0,1)}GRACE HOPER!`;
+const shortGreeting =`Welcome, ${currentUser.slice(0, 1)}!`;
